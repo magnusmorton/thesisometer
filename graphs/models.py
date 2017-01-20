07a9bc8c70b93f60fc78b38e8cfcd28811dd20b9
Magnus Morton <magnusmorton@icloud.com>
2017-01-20 23:40:24 +0000

    uniqueness behaviour added

--- graphs/models.py
+++ graphs/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -9,3 +10,17 @@ class WordCount(models.Model):
     count = models.IntegerField(default=0)
     date = models.DateField(default=datetime.date.today)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="counts")
+
+    def save(self, *args, **kwargs):
+        try:
+            self.validate_unique()
+        except ValidationError:
+            wc = WordCount.objects.filter(date=self.date, user=self.user).first()
+            wc.count = self.count
+            wc.save()
+            return
+        super(WordCount, self).save(*args, **kwargs)
+
+
+    class Meta:
+        unique_together = ("date", "user")
--- graphs/tests.py
+++ graphs/tests.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
+
+from .models import WordCount
 # Create your tests here.
 
 
@@ -11,12 +13,25 @@ class CountTestCase(TestCase):
         self.user = User.objects.create_user("bob")
         self.client = APIClient()
         self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_can_post_count(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.post('/graphs/api/wordcount/', {"count": 420}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_can_get_count(self):
         response = self.client.get('/graphs/api/wordcount/', format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_only_one_count_for_day(self):
+        WordCount(count=420, date="2017-01-20", user=self.user).save()
+        WordCount(count=500, date="2017-01-20", user=self.user).save()
+        counts = WordCount.objects.filter(date="2017-01-20")
+        self.assertEqual(len(counts), 1)
+
+    def test_can_make_multiple_counts(self):
+        WordCount(count=420, date="2017-01-20", user=self.user).save()
+        WordCount(count=500, date="2017-01-21", user=self.user).save()
+        counts = WordCount.objects.all()
+        self.assertEqual(len(counts), 2)
+
