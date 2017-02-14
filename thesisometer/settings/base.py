3d6f37fdfe10fcf8715576e9b234f00b4482e1a6
Magnus Morton <magnusmorton@icloud.com>
2017-02-14 21:39:45 +0000

    refactored settings

--- thesisometer/settings/base.py
+++ thesisometer/settings/base.py
@@ -13,17 +13,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k94ds!sk9)40^cbr2%r)00sck^*@9$$p*7=7e1i&cp_)%^9s@#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
---
+++ thesisometer/settings/dev.py
@@ -0,0 +1,8 @@
+from .base import *
+
+# dev only secret key
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'k94ds!sk9)40^cbr2%r)00sck^*@9$$p*7=7e1i&cp_)%^9s@#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
